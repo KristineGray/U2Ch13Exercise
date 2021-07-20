364613d32f6a0078017d60a1ac2b603aff1d0bca
KristineGray <krisgraya@gmail.com>
2021-07-20 15:03:27 -0400

    initial commit

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        private static List<string> Events = new List<string>();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+            return Redirect("/Events");
+        }
+    }
+}

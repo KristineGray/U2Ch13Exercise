a13bb812fa1880b80ca24f76652bcd4455ec852b
KristineGray <krisgraya@gmail.com>
2021-07-21 22:36:59 -0400

    created private static Dictionary for Events

--- CodingEvents/Data/EventData.cs
+++ CodingEvents/Data/EventData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CodingEvents.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,15 @@ namespace CodingEvents.Data
 {
     public class EventData
     {
+        // store events
+        private static Dictionary<int, Event> Events = new Dictionary<int, Event>();
+
+        // add events
+
+        // retrieve events
+
+        // retrieve single event
+
+        // remove event
     }
 }

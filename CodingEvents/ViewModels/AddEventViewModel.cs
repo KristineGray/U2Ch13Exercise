91c5879df84d9ec4c6d05ae9a8d41e66b9382a3b
KristineGray <krisgraya@gmail.com>
2021-07-28 12:14:47 -0400

    added props Location and NumAttending to AddEventViewModel.cs, added Required attributes to both, and Range attribute to NumAttending

--- CodingEvents/ViewModels/AddEventViewModel.cs
+++ CodingEvents/ViewModels/AddEventViewModel.cs
@@ -18,5 +18,12 @@ namespace CodingEvents.ViewModels
 
         [EmailAddress]
         public string ContactEmail { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        [Range(0, 100000)]
+        public int NumAttending { get; set; }
     }
 }

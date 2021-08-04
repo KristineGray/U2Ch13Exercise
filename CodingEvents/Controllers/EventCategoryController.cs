bcfc1d8d006d0cbe60d936302a61f1f671d5764f
KristineGray <krisgraya@gmail.com>
2021-08-04 12:54:12 -0400

    added EventDbContext instance and list to EventCategoryController and Index, & created Index view for EventCategoryController

---
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -0,0 +1,20 @@
+﻿using CodingEvents.Data;
+using CodingEvents.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEvents.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext _context;
+        public IActionResult Index()
+        {
+            List<EventCategory> eventCategories = _context.EventCategories.ToList();
+            return View(eventCategories);
+        }
+    }
+}

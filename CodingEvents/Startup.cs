83b1134268b9e2ab8e1dee0286aa47fa75dcabdd
KristineGray <krisgraya@gmail.com>
2021-08-04 09:37:54 -0400

    registered data store in ConfigureServices in Startup.cs

--- CodingEvents/Startup.cs
+++ CodingEvents/Startup.cs
@@ -1,5 +1,7 @@
+using CodingEvents.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,11 @@ namespace CodingEvents
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            string connectionString = "server=localhost;userid=coding_events;password=password;database=coding_events;";
+            MySqlServerVersion serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
+
+            services.AddDbContext<EventDbContext>(options =>
+                options.UseMySql(connectionString, serverVersion));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
